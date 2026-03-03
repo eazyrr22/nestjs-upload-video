8f7f4bf1c9c8ef726f4afee0d9dc120142399fc4
eazy <ezrirot@gmail.com>
2026-03-03 15:54:13 +0200

    env config and validation for db connection and path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import envConfig from './config/envConfig';
+import { envValidationSchema } from './config/env.validation';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load: [envConfig],
+    validationSchema: envValidationSchema,
+}) ],
 })
 export class AppModule {}
