9ccfb925ae730b25707fb3291fc1bf459f1ba4e6
eazy <ezrirot@gmail.com>
2026-03-05 20:07:42 +0200

    update env var for ffmpeg path

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -3,6 +3,7 @@ import Joi from "joi";
 export const envValidationSchema = Joi.object({
     PORT: Joi.number().default(3000),
     MONGO_URI: Joi.string().required(),
+    FFMPEG_PATH: Joi.string().required(),
     MONGO_DB_NAME: Joi.string().required(),
     LOCAL_STORAGE_PATH: Joi.string().required(),
     VIDEO_CODEC: Joi.string().default('libx264'),
--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -1,5 +1,6 @@
 export default () => ({
     port: Number(process.env.PORT) || 3000,
+    ffmpegPath: process.env.FFMPEG_PATH,
     database: {
         mongoUri: process.env.MONGO_URI,
         mongoDbName: process.env.MONGO_DB_NAME
