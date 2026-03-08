f03f2191dc7f3aebeeab75ccac57852331a83d67
eazy <ezrirot@gmail.com>
2026-03-08 18:30:41 +0200

    fix : update Storage Register module to import mongoose module for accessing the connection

--- src/databases/storageRegister.module.ts
+++ src/databases/storageRegister.module.ts
@@ -1,16 +1,25 @@
+import MongooseModule from '@nestjs/mongoose';
 import { DynamicModule, Module, Global } from '@nestjs/common';
-import { MongoService } from './mongoStorage.service';
+
 import { FsService } from './fsStorage.service';
 import { STORAGE_TOKEN } from './storage.interface';
+import { MongoService } from './mongoStorage.service';
 
 @Global()
 @Module({})
 export class StorageRegister {
     static register(storageType: 'mongo' | 'fs'): DynamicModule {
+        const dependencyModules: any[] = [];
+
+        if (storageType === 'mongo') {
+            dependencyModules.push(MongooseModule);
+        }
+
         const selectedProvider = (storageType === 'mongo') ? MongoService : FsService;
 
         return {
             module: StorageRegister,
+            imports: dependencyModules,
             providers: [selectedProvider, {
                 provide: STORAGE_TOKEN,
                 useExisting: selectedProvider
