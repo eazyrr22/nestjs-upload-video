68cc9bfc7a9f488e292aaccf40c77e20a72a7389
eazy <ezrirot@gmail.com>
2026-03-11 16:57:13 +0200

    new : deleteFile method test

--- src/fileStorage/s3.spec.ts
+++ src/fileStorage/s3.spec.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs-extra';
 import { ConfigService } from '@nestjs/config';
 import { mockClient } from 'aws-sdk-client-mock';
 import { Test, TestingModule } from '@nestjs/testing';
-import { S3Client, PutObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand, DeleteObjectCommand, DeleteObjectTaggingCommand } from '@aws-sdk/client-s3';
 
 import { S3FileService } from './s3.service';
 
@@ -57,7 +57,18 @@ describe('S3FileService', () => {
             mockS3Client.on(PutObjectCommand).rejects(new Error('Connection error'));
             await expect(s3Service.saveFile(mockFilePath)).rejects.toThrow('Failed to upload file to S3');
         });
+        it('should throw error if file exceeds size limit', async () => {
+            jest.spyOn(fs, 'stat').mockResolvedValue({size: 5 * 1024 * 1024 * 1024 }as never);
+
+            await expect(s3Service.saveFile('large-file.mp4'))
+                .rejects
+                .toThrow('File too large');
+
+            expect(mockS3Client).not.toHaveReceivedCommand(PutObjectCommand);
+        });
+
         afterEach(() => {
+            jest.restoreAllMocks();
             expect(mockS3Client).toHaveReceivedCommandWith(PutObjectCommand, {
                 Bucket: 'test-bucket',
                 Key: 'test.mp4',
@@ -67,5 +78,25 @@ describe('S3FileService', () => {
             expect(mockS3Client.calls()).toHaveLength(1);
         });
     });
-});
 
+    describe('deleteFile', () => {
+        const mockFileName = 'test.mp4';
+
+        it('should delete the file', async () => {
+            mockS3Client.on(DeleteObjectCommand).resolves({});
+            await expect(s3Service.deleteFile(mockFileName)).resolves.toBeUndefined();
+        });
+        it('should throw an error if delete fails - s3 error ', async () => {
+            mockS3Client.on(DeleteObjectCommand).resolves({ $metadata: { httpStatusCode: 500 } });
+            await expect(s3Service.saveFile(mockFileName)).rejects.toThrow('Failed to upload file to S3');
+        });
+        afterEach(() => {
+            expect(mockS3Client).toHaveReceivedCommandWith(DeleteObjectCommand, {
+                Bucket: 'test-bucket',
+                Key: 'test.mp4',
+            });
+            expect(mockS3Client.calls()).toHaveLength(1);
+        });
+    });
+
+});
