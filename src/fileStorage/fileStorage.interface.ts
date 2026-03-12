04fc5372317c6a67947e2050b645bd23e2f8cbff
eazy <ezrirot@gmail.com>
2026-03-12 11:07:12 +0200

    fix : expose s3 client as a module

--- src/fileStorage/fileStorage.interface.ts
+++ src/fileStorage/fileStorage.interface.ts
@@ -2,4 +2,5 @@ export interface IFileStorage {
     saveFile(sourceFilePath: string): Promise<string>;
     deleteFile(fileName: string): Promise<void>;
 }
-export const FILE_STORAGE_TOKEN = 'FILE_STORAGE_TOKEN';
+export const FILE_STORAGE_TOKEN = 'FILE_STORAGE_TOKEN'
+, S3_CLIENT_TOKEN = 'S3_CLIENT_TOKEN';
